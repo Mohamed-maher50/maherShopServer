605f7f7ed9c3cd5ca36eb829af980e87efb799f7
mohamed maher <mohamedmaher.mm330@gmail.com>
2023-12-17 15:14:06 +0200

    upload server

---
+++ routes/Auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  signUp,
+  login,
+  ForgetPass,
+  verifyPass,
+  resetPassword,
+} = require("../controller/C_auth");
+const { V_Create, V_login } = require("../validation/V_user");
+
+router.post("/signup", V_Create, signUp);
+router.post("/signIn", V_login, login);
+router.post("/forgotPasswords", ForgetPass);
+router.post("/verifyResetCode", verifyPass);
+router.put("/resetPassword", resetPassword);
+
+module.exports = router;
